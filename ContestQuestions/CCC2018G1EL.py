c6af009bf399001c991ae3b663d10767c5a69804
Ethan Li <ethan.li@Y22.local>
2018-11-19 08:45:49 -0500

    Add ds

---
+++ ContestQuestions/CCC2018G1EL.py
@@ -0,0 +1,17 @@
+#input
+a = input("")
+a = int(a)
+
+b = input("")
+b = int(b)
+
+c = input("")
+c = int(b)
+
+c = input("")
+c = int(b)
+
+if (a == 8 or a == 9) and (b == c) and (d == 8 or d ==9 )
+print ("ignore")
+else 
+print ("Answer")
